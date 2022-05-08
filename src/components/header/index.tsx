21bd94d95a68309ce929e3c9e28863bfa7f84db7
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-05-08 15:45:16 +0530

    Fixed header profile icon css

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -18,9 +18,9 @@ const Header: NextPage = () => {
             <div className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4 cursor-pointer">Examples</div>
             <div className="block mt-4 lg:inline-block lg:mt-0 text-white cursor-pointer">Blog</div>
           </div>
-          <div className="flex flex-row">
-            <Image src="/profile.png" alt="Mangashelf" width={30} height={30} className="cursor-pointer rounded-full bg-white min-h-0" />
-            <div className="ml-4 inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0 items-center justify-center cursor-pointer">
+          <div className="flex">
+            <Image src="/profile.png" alt="Mangashelf" width={40} height={40} className="cursor-pointer rounded-full bg-white py-2 items-center justify-center" />
+            <div className="ml-4 text-sm px-4 py-2 border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0 items-center justify-center cursor-pointer">
               Logout
             </div>
           </div>

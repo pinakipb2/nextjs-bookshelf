b166b99450392ec676af4d861cbfd62caf3b1895
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-05-14 22:12:24 +0530

    Converted REVALIDATE_IN into function

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,6 @@
+// Variables
 export const TOTAL_MANGAS: number = 213;
-export const REVALIDATE_IN: number = 120;
 export const PAGE_LIMIT: number = 5;
+
+// Functions
+export const REVALIDATE_IN: (time_in_mins: number) => number = (time_in_mins: number): number => time_in_mins * 60;

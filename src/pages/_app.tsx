b70ec09950ff0a7094e23cb0c54823875edd37b0
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-05-15 18:09:04 +0530

    Changed queryClient from state to const

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  // const [queryClient] = useState(() => new QueryClient());
+  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />

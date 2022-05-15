265130b6a12eb8884be4d1dcff60a6fd41176652
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-05-15 20:47:40 +0530

    Updated types

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const TOTAL_MANGAS: number = 213;
 export const PAGE_LIMIT: number = 5;
 
 // Functions
-export const REVALIDATE_IN: (time_in_mins: number) => number = (time_in_mins: number): number => time_in_mins * 60;
+export const REVALIDATE_IN = (time_in_mins: number): number => time_in_mins * 60;
